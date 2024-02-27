5b8d0eeec7b0629ae0954980a3fd218ffc6fc12c
Ayushi Rai <ayushicodes@gmail.com>
2024-02-27 10:39:48 +0530

    Slider removed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header1 from "./component/Header1";
 import Header2 from "./component/Header2";
 import Header from "./component/Header";
 import PlaceImages from "./component/PlaceImages";
+// import { Slider } from "./Slider";
+// import slider from "./component/Mock";
 
 function App() {
   return (
@@ -12,16 +14,29 @@ function App() {
       <Header2 />
       <LandingPage />
       <Header heading="Popular Destination" />
-      <div className="max-w[1320px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-3 justify-items-center mt-20">
         {PlaceImages.map((items) => {
           console.log("url", items.url);
           return (
-            <div>
-              <div>
-                <img src={items.url} alt={items.alt} className="h-full " />
+            <div className="py-10">
+              <div className=" overflow-hidden max-w-sm shadow-lg rounded">
+                <img src={items.url} alt={items.alt} className="w-full " />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{items.name}</div>
+                  <p className="text-gray-400	">{items.decs}</p>
+                </div>
+                <div className="grid grid-flow-col gap-5 pb-2 px-6">
+                  <span className="bg-gray-200 rounded-full px- text-sm py-1 font-base mb-2">
+                    #Luxry
+                  </span>
+                  <span className="bg-gray-200 rounded-full px- text-sm py-1 font-base mb-2">
+                    #Hotel
+                  </span>
+                  <span className="bg-gray-200 rounded-full px- text-sm py-1 font-base mb-2">
+                    #Travel
+                  </span>
+                </div>
               </div>
-              <h5>{items.decs}</h5>
-              <button>Read More</button>
             </div>
           );
         })}
---
+++ src/component/Mock.js
@@ -0,0 +1,14 @@
+// [
+//   {
+//     url: "./../Assets/Goa.png",
+//   },
+//   {
+//     url: "./../Assets/Kashmir.png",
+//   },
+//   {
+//     url: "./../Assets/Manali.png",
+//   },
+//   {
+//     url: "./../Assets/Shimla.png",
+//   },
+// ];
