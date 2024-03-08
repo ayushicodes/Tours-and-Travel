7314f6bbda4ca327d2c09d96b176a7573829b9e5
Ayushi Rai <ayushicodes@gmail.com>
2024-03-08 21:07:32 +0530

    button text changed

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -20,15 +20,15 @@ const Footer = () => {
         <div>
           <input
             type="text"
-            placeholder="Enter Your ph.no"
+            placeholder="Phone number"
             className="text-gray-800
            sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
           />
           <button
-            className="bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[Poppins]
+            className="bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2 font-[Poppins]
            rounded-md text-white md:w-auto w-full"
           >
-            Request Booking
+            Enquire Now
           </button>
         </div>
       </div>
