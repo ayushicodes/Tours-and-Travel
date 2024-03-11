950e9f2584e1c887aab294b1740e89a4537c4ee4
Ayushi Rai <ayushicodes@gmail.com>
2024-03-12 00:39:51 +0530

    footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -37,7 +37,7 @@ const Footer = () => {
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
       text-center pt-2 text-gray-400 text-sm pb-8"
       >
-        <span>© 2024 Appy. All rights reserved.</span>
+        <span>© 2024 Apply. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
         {/* <SocialIcons Icons={Icons} /> */}
       </div>
